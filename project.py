9643539489c9e9a540912442e9ce5dadf0e4fb1b
Aqeel <aqeelvahmed@gmail.com>
2024-06-13 15:05:37 +0100

    Update project.py

--- project.py
+++ project.py
@@ -13,32 +13,58 @@ headers = {"Authorization" : "Bearer " + API_KEY,
 
 
 
+
+
+### --- PSN PROFILES TROPHY_LIST SCRAPER --- ###
+def trophy_scraper(url):
+    driver = webdriver.Firefox()
+    driver.get(url)
+    sleep(2)
+    
+    iframe = driver.find_element(By.CSS_SELECTOR, "iframe[src='https://cdn.privacy-mgmt.com/index.html?hasCsp=true&message_id=1140375&consentUUID=null&consent_origin=https%3A%2F%2Fcdn.privacy-mgmt.com%2Fconsent%2Ftcfv2&preload_message=true&version=v1']")
+    driver.switch_to.frame(iframe)  # Cookies in Iframe, so we switch to this
+    button = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[3]/div[2]/button[1]')
+    button.click()  # Click button inside Iframe
+    driver.switch_to.default_content()
+    
+    trophy = driver.find_elements(By.CSS_SELECTOR, "td[style='width: 100%;']")
+    trophy = [t.text for t in trophy]  # No Details gathered in this scenario
+
+    game_name = driver.find_element(By.XPATH, '/html/body/div[4]/div[3]/div/div[2]/div[1]/div[2]').text.title()
+    driver.quit()
+    return (trophy, game_name, 1)
+
+
+
+
 ### --- PSN PROFILES SCRAPER --- ###
 def psnprofiles_scraper(url):
     driver = webdriver.Firefox()
     driver.get(url)
     sleep(2)
-    
-    # Cookies is in Iframe so click button from there
-    iframe = driver.find_element(By.CSS_SELECTOR, "iframe[src='https://cdn.privacy-mgmt.com/index.html?hasCsp=true&message_id=1126540&consentUUID=null&preload_message=true&version=v1']")
-    driver.switch_to.frame(iframe) 
-    button = driver.find_element(By.XPATH, "/html/body/div/div[2]/div[3]/div[2]/button")
-    button.click()
-    driver.switch_to.default_content()        
-
-    a = driver.find_elements(By.CLASS_NAME, 'grow')  # Detects Tips & Strategies section and removes it from details
-    a = [t.text for t in a]
+        
+    iframe = driver.find_element(By.CSS_SELECTOR, "iframe[src='https://cdn.privacy-mgmt.com/index.html?hasCsp=true&message_id=1140375&consentUUID=null&consent_origin=https%3A%2F%2Fcdn.privacy-mgmt.com%2Fconsent%2Ftcfv2&preload_message=true&version=v1']")  # Might have to change this link
+    driver.switch_to.frame(iframe)  # Cookies in Iframe, so we switch to this
+    button = driver.find_element(By.XPATH, "/html/body/div/div[2]/div[3]/div[2]/button[1]")
+    button.click()  # Click button in Iframe
+    driver.switch_to.default_content()
      
-    trophy = driver.find_elements(By.CSS_SELECTOR, "td[style*='padding']") # Gets Trophy Name & Description
+    trophy = driver.find_elements(By.CSS_SELECTOR, "td[style*='padding']")  # Gets Trophy Name & Description
     details = driver.find_elements(By.CLASS_NAME, 'section-original')  # Gets Trophy Specific Guide
-
     trophy = [t.text for t in trophy]
     details = [d.text[:1500] for d in details]    
-    details = details[1:].insert(0, ' ') if a.index('GUIDE CONTENTS') - a.index('ROADMAP') !=1 else details.insert(0, ' ')
-
+    
+    a = driver.find_elements(By.CLASS_NAME, 'grow')  # Detects Tips & Strategies section and removes it from details
+    a = [t.text for t in a]
+    try:
+        details = details[1:] if a.index('GUIDE CONTENTS') - a.index('ROADMAP') !=1 else details
+        details.insert(0, ' ')  # Outside to ensure no errors
+    except:
+        pass
+    
     game_name = driver.find_element(By.XPATH, '//*[@id="desc-name"]').text
     driver.quit()
-    return (trophy, details, game_name)
+    return (trophy, game_name, 2, details)
 
 
 
@@ -49,12 +75,8 @@ def powerpyx_scraper(url):
     driver.get(url)
     sleep(3)
     
-    try:
-        button = driver.find_element(By.CLASS_NAME, 'fc-button')  # Handle Cookie button
-        button.click() 
-    except:
-        print('failure')
-        pass
+    button = driver.find_element(By.CLASS_NAME, 'fc-button')  # Handle Cookie button
+    button.click()
     
     trophy = driver.find_elements(By.CSS_SELECTOR, 'tr > td:nth-child(2)')
     details = driver.find_elements(By.CSS_SELECTOR, 'tr > td[colspan="3"]')
@@ -63,14 +85,22 @@ def powerpyx_scraper(url):
     
     game_name = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div/div[3]/main/article/div/h2[1]').text
     driver.quit()
-    return (trophy, details, game_name)
+    return (trophy, game_name, 2, details)
 
 
 
 
 
 
-### --- CHECK GAME --- ###
+### --- ADDING INTO NOTION --- ###
+def tracker_adder(url, DB_ID): 
+    check_game(DB_ID, game_name)
+    
+    for key in data:
+        add_row(DB_ID, data[key])
+
+
+### --- CHECK GAME NAME EXISTS IN NOTION --- ###
 def check_game(DB_ID, game_name):  # Check if game_name exists
     res = requests.get(f'https://api.notion.com/v1/databases/{DB_ID}', headers=headers)
     db_info = res.json()
@@ -82,22 +112,29 @@ def check_game(DB_ID, game_name):  # Check if game_name exists
         payload = {"parent": {"database_id": DB_ID},
         "properties": {"Game": {"type": "select", "select": {"name": game_name}}}}
         res = requests.post('https://api.notion.com/v1/pages', json=payload, headers=headers)        
- 
-
-
 
 
 ### --- ADD ROW --- ####
 def add_row(DB_ID, data):
-    payload = {
-        "parent": {"database_id": DB_ID},
-        "properties": {
-            "Trophy": {"title": [{"text": {"content": data["Trophy"]}}]},
-            "Details": {"rich_text": [{"text": {"content": data["Details"]}}]},
-            "Notes": {"rich_text": [{"text": {"content": data["Notes"]}}]},
-            "Game": {"select": {"name": data["Game"]}}
-        }}
-    
+    if returned[2] == 2:  # 2 Means that Notes are gathered and need to be submitted
+        payload = {        
+            "parent": {"database_id": DB_ID},
+            "properties": {
+                "Trophy": {"title": [{"text": {"content": data["Trophy"]}}]},
+                "Details": {"rich_text": [{"text": {"content": data["Details"]}}]},
+                "Notes": {"rich_text", [{"text": {"content": data["Notes"]}}]},
+                "Game": {"select": {"name": data["Game"]}}
+            }}
+    elif returned[2] == 1:  # No Notes so can omit here
+        payload = {        
+            "parent": {"database_id": DB_ID},
+            "properties": {
+                "Trophy": {"title": [{"text": {"content": data["Trophy"]}}]},
+                "Details": {"rich_text": [{"text": {"content": data["Details"]}}]},
+                "Game": {"select": {"name": data["Game"]}}
+            }}
+
+        
     for attempt in range(3):
         res = requests.post(f"https://api.notion.com/v1/pages", json=payload, headers=headers)
         if res.status_code == 200:
@@ -112,38 +149,45 @@ def add_row(DB_ID, data):
 
 
 ### --- TRACKER ADDER --- ####
-def tracker_adder(url, DB_ID): 
-    check_game(DB_ID, game_name)
-    
-    for key in data:
-        add_row(DB_ID, data[key])
+url = ''
 
-
-
-url = 'https://psnprofiles.com/guide/14008-elden-ring-trophy-guide'
-if 'powerpyx' in url: # Automatically choses appropriate scraper from url-link
+if 'powerpyx' in url and 'guide' in url: # Automatically choses appropriate scraper from url-link
     returned = powerpyx_scraper(url)
-elif 'psnprofiles' in url:
+elif 'psnprofiles' in url and 'guide' in url:
     returned = psnprofiles_scraper(url)
+else:  # This scrapes just trophy list
+    returned = trophy_scraper(url)
 
 
 
-
-trophy_list = [t.split('\n') for t in returned[0]] # Fixes Format
+trophy_list = [t.split('\n', maxsplit=1) for t in returned[0]] # Splits by first \n 
 trophy_list = [t for t in trophy_list if len(t)>1] # Removes irrelevant lists
 trophy_list = [i for i in trophy_list if all(x not in i[0] for x in ['Trophy Guide', 'Collectibles', 'Walkthrough', '100%'])]  # Removes Extra pick ups
 
-details_list = [d.replace('\n', ' ') for d in returned[1]] # Fixes Format
+game_name = returned[1]
+for x in [' Trophy Roadmap', ' Trophy Guide', ' Trophies']:
+    game_name = game_name.replace(x, '')
 
-game_name = returned[2].replace(' Trophy Roadmap', '')  # Ensures only name captured
-game_name = returned[2].replace(' Trophy Guide', '')
+if returned[2] == 2:
+    details_list = [d.replace('\n', ' ')[:1500] for d in returned[3]] # Fixes Format
 
 
 
 length = len(trophy_list)
 data = {}
-for i in range(0,length):
-    data[i] = {'Trophy': trophy_list[i][0], 'Details': trophy_list[i][1], 'Notes': details_list[i][:1500], 'Game': game_name}
+if returned[2] == 2:
+    print(1)
+    for i in range(0, length):
+        data[i] = {'Trophy': trophy_list[i][0],
+                   'Details': trophy_list[i][1],
+                   'Notes': details_list[i],
+                   'Game': game_name}
+elif returned[2] == 1:
+    print(2)
+    for i in range(0, length):
+        data[i] = {'Trophy': trophy_list[i][0],
+                   'Details': trophy_list[i][1],
+                   'Game': game_name}
 
 tracker_adder(url, DB_ID)
 
